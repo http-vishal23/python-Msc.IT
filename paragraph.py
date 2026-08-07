36adab9b4c1f2c138e35bc931b7b35d868353c3a
http-vishal23 <sarvaiyavishal6164@gmail.com>
2026-08-07 21:32:55 +0530

    Update paragraph.py

--- paragraph.py
+++ paragraph.py
@@ -1,79 +1,29 @@
 para = input("Enter a paragraph: ")
 
-punctuation = ".,!?;:'\"()-[]{}<>/\\@#$%^&*_+=|`~"
+words = para.split()
 
-clean_text = ""
-for ch in para:
-    if ch not in punctuation:
-        clean_text += ch
-    else:
-        clean_text += " "
+print("Total words:", len(words))
 
-print("\nParagraph without punctuation:")
-print(clean_text)
-
-words = clean_text.split()
-
-word_list = []
+unique = []
 for word in words:
-    word_list.append(word.lower())
+    if word not in unique:
+        unique.append(word)
 
-print("\n1. Total number of words:", len(word_list))
+print("Unique words:", len(unique))
 
-unique_words = []
-for word in word_list:
-    if word not in unique_words:
-        unique_words.append(word)
+longest = words[0]
+for word in words:
+    if len(word) > len(longest):
+        longest = word
+print("Longest word:", longest)
 
-print("2. Number of unique words:", len(unique_words))
+shortest = words[0]
+for word in words:
+    if len(word) < len(shortest):
+        shortest = word
+print("Shortest word:", shortest)
 
-max_len = len(word_list[0])
-for word in word_list:
-    if len(word) > max_len:
-        max_len = len(word)
-
-print("3. Longest word(s):")
-for word in unique_words:
-    if len(word) == max_len:
+print("Repeated words:")
+for word in unique:
+    if words.count(word) > 1:
         print(word)
-
-min_len = len(word_list[0])
-for word in word_list:
-    if len(word) < min_len:
-        min_len = len(word)
-
-print("4. Shortest word(s):")
-for word in unique_words:
-    if len(word) == min_len:
-        print(word)
-
-print("5. Words appearing more than once:")
-found = False
-for word in unique_words:
-    count = 0
-    for w in word_list:
-        if word == w:
-            count += 1
-    if count > 1:
-        print(word, "->", count, "times")
-        found = True
-
-if not found:
-    print("No repeated words")
-
-sorted_words = sorted(unique_words)
-print("\nWords in alphabetical order:")
-for word in sorted_words:
-    print(word)
-
-search = input("\nEnter a word to search: ").lower()
-
-positions = []
-for i in range(len(word_list)):
-    if word_list[i] == search:
-        positions.append(i)
-
-if positions:
-    print("Word found at positions:", positions)
-else:
-    print("Word not found.")
